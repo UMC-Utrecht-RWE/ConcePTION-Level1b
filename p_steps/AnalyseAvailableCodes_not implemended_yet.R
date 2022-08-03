3681f233ffa1cf00cd3302e5416b0b0b67c05949
relbersumcu <r.j.h.elbers@umcutrecht.nl>
2022-08-03 14:07:31 +0200

    Working on query sql performnce

--- p_steps/AnalyseAvailableCodes_not implemended_yet.R
+++ p_steps/AnalyseAvailableCodes_not implemended_yet.R
@@ -1,37 +1,36 @@
 
 
-#Empty memory
-rm(list=ls())
-gc()
 
 #To fill
 ###
 #DAP <- "SIDIAP"
-path <- "H:/Review_level1b/"
-path_code <- "C:/C4591021_PfizerUMC/Data characterisation/PfizerScript/p_meta_data/Pfizer_full_codelist.csv"
-TABLE <- "EVENTS"
+path_code <- paste0(projectFolder,"/p_meta/")
+
 ###
 
-
+TABLE <- "EVENTS"
 c.voc = "coding_system"
 c.concept = "Outcome"
 c.codes = "code"
 f.code = "event_code"
 f.voc =  "event_record_vocabulary"
 c.startwith = c("ICD10CM","ICD10","ICD10DA","ICD9CM","MTHICD9")
-
+n.codesheet = "ALL_full_codelist.csv"
 
 
 #dbcdm <- "H:/Review_level1b/tempdb.db"
 
 #codesheet <- readRDS(paste0(tmp,"CODES_EVENTS.rds"))
 
+codesheet <- IMPORT_PATTERN(pat = n.codesheet, dir = path_code)
 
 
-codesheet <-  fread(path_code)
-codesheet <-  codesheet[, Outcome := paste0(system,"_",event_abbreviation,"_",type)]
+if(TABLE == "EVENTS"){
+  codesheet <-  codesheet[, Outcome := paste0(system,"_",event_abbreviation,"_",type)]
+  setnames(codesheet,c(c.voc,c.concept,c.codes),c("Type","Concept","Code"))
+}
 
-setnames(codesheet,c(c.voc,c.concept,c.codes),c("Type","Concept","Code"))
+
 
 
 #Create variable code_no_dot by removing dot from all codes
@@ -42,57 +41,72 @@ codesheet[,start_with := fifelse(substr(Code,nchar(Code),nchar(Code) + 1) == "."
 
  
 
-files <- list.files(path, include.dirs = T, all.files = T, full.names = T, recursive = T)
+files <- list.files(paste0(projectFolder,"/g_intermediate/"), include.dirs = T, all.files = T, full.names = T, recursive = T)
 
-files <- files[grepl(paste0("WHERECLAUSE_", TABLE), files)]
-i=files[1]
-report <- list()
-
-for(i in files){
+file <- files[grepl(paste0("WHERECLAUSE_", TABLE), files)]
 
 
-        mydb <- dbConnect(RSQLite::SQLite(), "")
+        dbListTables(mydb)
+
+        mydb <- dbConnect(RSQLite::SQLite(), paste0(projectFolder,"/g_intermediate/database.db"))
+
+        tmpdb <- dbConnect(RSQLite::SQLite(), "")
         
-        dbWriteTable(mydb, "codesheet" ,codesheet[,c("Type","Concept","Code", "code_no_dot", "start_with") , with = F], overwrite = T, append = F)
+        dbWriteTable(tmpdb, "codesheet" ,codesheet[,c("Type","Concept","Code", "code_no_dot", "start_with") , with = F], overwrite = T, append = F)
         
-        DATA_FILE <- fread(i)
+        DATA_FILE <- dbGetQuery(mydb, paste0("SELECT DISTINCT ",f.code,",", f.voc," FROM ", TABLE))
         
-        dbWriteTable(mydb, "TEMP" ,DATA_FILE , overwrite = T, append = F) 
-        
-        p <- dbSendStatement(mydb, paste0("CREATE TABLE TEMP2 AS SELECT DISTINCT ",f.voc," AS Type2, REPLACE( ",f.code,", '.', '' ) as code_no_dot2 FROM TEMP"))
+        dbWriteTable(tmpdb, "TEMP" ,DATA_FILE , overwrite = T, append = F) 
+        dbGetQuery(tmpdb, "SELECT * FROM TEMP")
+        p <- dbSendStatement(tmpdb, paste0("CREATE TABLE TEMP2 AS SELECT DISTINCT ",f.voc," AS Type2, REPLACE( ",f.code,", '.', '' ) as code_no_dot2 FROM TEMP"))
         dbClearResult(p)
         
-        p <- dbSendStatement(mydb, paste0("CREATE INDEX TEMP2_index ON TEMP2( code_no_dot2, Type2)"))
+        p <- dbSendStatement(tmpdb, paste0("CREATE INDEX TEMP2_index ON TEMP2( Type2, code_no_dot2 )"))
         dbClearResult(p)
         
-        p <- dbSendStatement(mydb, paste0("CREATE INDEX codesheet_index ON codesheet( code_no_dot, Type, start_with)"))
+        p <- dbSendStatement(tmpdb, paste0("CREATE INDEX codesheet_index ON codesheet( code_no_dot, Type, start_with)"))
         dbClearResult(p)
         
-        voc <- paste0(dbGetQuery(mydb, "SELECT DISTINCT Type2 FROM TEMP2")[["Type2"]], collapse = "|")
+        voc <- paste0(dbGetQuery(tmpdb, "SELECT DISTINCT Type2 FROM TEMP2")[["Type2"]], collapse = "|")
         
-        system.time(TEMP1 <- dbGetQuery(mydb,"
-                  select  t1.*, t2.Concept
-                  from TEMP2 t1
-                  inner join codesheet t2
+        #Problem to solve
+        ###
+        #https://stackoverflow.com/questions/59409075/sqlite-like-operator-is-very-slow-compared-to-the-operator
+        #t1.code_no_dot2 LIKE(t2.code_no_dot || '%')
+        ###
         
-                  on (
+        aatest <- dbGetQuery(tmpdb, "SELECT DISTINCT * FROM TEMP2")
+        #EXPLAIN QUERY PLAN
+        system.time(TEMP14 <- dbGetQuery(tmpdb,"
+                  
+                  EXPLAIN QUERY PLAN
+                  SELECT DISTINCT  t1.Type2 , t2.Concept
+                  FROM (SELECT DISTINCT * FROM TEMP2) t1
+                  INNER JOIN codesheet t2
+        
+                  ON (
                         t1.Type2 = t2.Type
         
-                        and
+                        AND
+                        
                         (
         
                               (
         
                               t2.start_with = 'T'
-                              and
+                              
+                              AND
+                              
                               substr(t1.code_no_dot2,1,length(t2.code_no_dot)) = substr(t2.code_no_dot,1,length(t2.code_no_dot))
                               )
         
-                              or
+                              OR
         
                               (
                               t2.start_with = 'F'
-                              and
+                              
+                              AND
+                              
                               t1.code_no_dot2 = t2.code_no_dot
                               )
                         )
